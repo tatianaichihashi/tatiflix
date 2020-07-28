474269ef7e9f61cca2f7315f3d0c1554e2c39a6a
Tatiana Ichihashi <tatiana.ichihashi@jolivi.com.br>
2020-07-28 19:39:00 -0300

    setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png'
+import './Menu.css';
+import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+            <img className="Logo" src={Logo} alt="Tatiflix logo"/>
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo Video
+            </ButtonLink>
+        </nav>
+    );
+}
+
+export default Menu; 
